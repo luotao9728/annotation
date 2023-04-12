1fbb4d2a33dd4109b1ca8df373a294d1898521dd
Tao Luo <112722913+luotao9728@users.noreply.github.com>
2023-04-12 13:13:33 -0400

    Update annotation.sh

--- annotation.sh
+++ annotation.sh
@@ -16,9 +16,28 @@ echo "The file name of illumina RNA-seq (reverse) is: $ill_rev"
 echo "The file name of PacBio RNA-seq is: $pacbio"
 echo "The file name of reference annotation is: $ref_annotation"
 
+#SBATCH -N 2
+#SBATCH -p RM
+#SBATCH -t 5:00:00
+#SBATCH --ntasks-per-node=128
+
+# Initiate bash environment
+source ~/.bashrc
+
+# Activate conda environment
+conda activate annotation
+
 # Start pipeline
 echo "Start annotaion pipeline!"
 
+# Start trimming illumina RNA-seq
 echo "Start trimming illumina RNA-seq!"
+trimmomatic PE -phred33 $ill_for $ill_rev output_forward_paired.fastq output_forward_unpaired.fastq output_reverse_paired.fastq output_reverse_unpaired.fastq ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
+echo "Finish trimming illumina RNA-seq!"
 
-# trimmomatic PE -phred33 $ill_for $ill_rev output_forward_paired.fastq output_forward_unpaired.fastq output_reverse_paired.fastq output_reverse_unpaired.fastq ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
+# Start correcting PacBio RNA-seq
+echo "Start trimming PacBio RNA-seq!"
+lordec-correct -2 /ocean/projects/bio230007p/tluo1/data_processing/trimmed_liver_illumina_R2.fastq -k 19 -s 3 -i /ocean/projects/bio230007p/tluo1/data/liver_pacbio/ERR9764407.fastq -o /ocean/projects/bio230007p/tayers/liver_pacbio_lordec_corrected.fasta
+echo "Finish trimming PacBio RNA-seq!"
+
+conda deactivate
