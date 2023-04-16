ccea69115a8bd5384e5d1b3d2f5fe651ccb9bc1c
Tao Luo <112722913+luotao9728@users.noreply.github.com>
2023-04-16 13:35:08 -0400

    Update annotation.sh

--- annotation.sh
+++ annotation.sh
@@ -1,43 +1,86 @@
 #!/bin/bash
-
-# Take inputs
-echo "**************************************************************"
-echo "Please enter the file name with the following prompts:"
-read -p "Reference genome (fasta/fna): " ref_genome
-read -p "illumina RNA-seq forward (fastq): " ill_for
-read -p "illumina RNA-seq reverse (fastq): " ill_rev
-read -p "PacBio RNA-seq (fastq): " pacbio
-read -p "Reference annotation (gtf): " ref_annotation
-echo "**************************************************************"
-
-echo "The file name of reference genome is: $ref_genome"
-echo "The file name of illumina RNA-seq (forward) is: $ill_for"
-echo "The file name of illumina RNA-seq (reverse) is: $ill_rev"
-echo "The file name of PacBio RNA-seq is: $pacbio"
-echo "The file name of reference annotation is: $ref_annotation"
-
-#SBATCH -N 2
+#SBATCH -N 4
 #SBATCH -p RM
-#SBATCH -t 5:00:00
+#SBATCH -t 40:00:00
 #SBATCH --ntasks-per-node=128
 
-# Initiate bash environment
+# activate local batch environment
 source ~/.bashrc
+echo "*******************************************************************************************"
+echo "Bash environment activated!"
+echo "*******************************************************************************************"
 
-# Activate conda environment
+# activate conda environment for the pipeline
 conda activate annotation
+echo "*******************************************************************************************"
+echo "Conda environment activated!"
+echo "*******************************************************************************************"
 
-# Start pipeline
-echo "Start annotaion pipeline!"
+# change direction to data location
+cd /ocean/projects/bio230007p/tluo1/data_processing_new
 
-# Start trimming illumina RNA-seq
-echo "Start trimming illumina RNA-seq!"
-trimmomatic PE -phred33 $ill_for $ill_rev output_forward_paired.fastq output_forward_unpaired.fastq output_reverse_paired.fastq output_reverse_unpaired.fastq ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
-echo "Finish trimming illumina RNA-seq!"
+# Trimmomatic
+echo "*******************************************************************************************"
+echo "Start trimming illumina short RNA-seq!"
+echo "*******************************************************************************************"
+trimmomatic PE -phred33 liver_illumina_R1.fastq liver_illumina_R2.fastq trimmed_liver_illumina_R1.fastq output_forward_unpaired.fq.gz trimmed_liver_illumina_R2.fastq output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36
 
-# Start correcting PacBio RNA-seq
-echo "Start trimming PacBio RNA-seq!"
-lordec-correct -2 /ocean/projects/bio230007p/tluo1/data_processing/trimmed_liver_illumina_R2.fastq -k 19 -s 3 -i /ocean/projects/bio230007p/tluo1/data/liver_pacbio/ERR9764407.fastq -o /ocean/projects/bio230007p/tayers/liver_pacbio_lordec_corrected.fasta
-echo "Finish trimming PacBio RNA-seq!"
+rm output_forward_unpaired.fq.gz
+rm output_reverse_unpaired.fq.gz
+echo "*******************************************************************************************"
+echo "Finish trimming illumina short RNA-seq!"
+echo "*******************************************************************************************"
 
+# LorDEC
+## lordec-correct
+echo "*******************************************************************************************"
+echo "Start correcting PacBio long RNA-seq!"
+echo "*******************************************************************************************"
+
+lordec-correct -2 trimmed_liver_illumina_R1.fastq trimmed_liver_illumina_R2.fastq -k 19 -s 3 -i liver_pacbio.fastq -o liver_pacbio_lordec_corrected.fasta
+
+echo "*******************************************************************************************"
+echo "Finish correcting PacBio long RNA-seq!"
+echo "*******************************************************************************************"
+
+## lordec-trim
+echo "*******************************************************************************************"
+echo "Start trimming corrected PacBio long RNA-seq!"
+echo "*******************************************************************************************"
+
+lordec-trim -i liver_pacbio_lordec_corrected.fasta -o trim_liver_pacbio.fasta
+
+echo "*******************************************************************************************"
+echo "Finish trimming corrected PacBio long RNA-seq!"
+echo "*******************************************************************************************"
+
+## lordec-trim-split
+echo "*******************************************************************************************"
+echo "Start trim-splitting corrected PacBio long RNA-seq!"
+echo "*******************************************************************************************"
+
+lordec-trim-split -i liver_pacbio_lordec_corrected.fasta -o trim_split_liver_pacbio.fasta
+
+echo "*******************************************************************************************"
+echo "Finish trim-splitting corrected PacBio long RNA-seq!"
+echo "*******************************************************************************************"
+
+# Magic-BLAST
+echo "*******************************************************************************************"
+echo "Start alignments! Please be patient, this might take few (or many) hours!"
+echo "*******************************************************************************************"
+
+mkdir alignments
+
+makeblastdb -in ref_genome_horse.fna -out horse_reference -parse_seqids -dbtype nucl
+
+magicblast -query trim_split_liver_pacbio.fasta -db horse_reference -out alignments/liver_align_pacbio.sam
+magicblast -query trimmed_liver_illumina_R1.fastq -db horse_reference -infmt fastq -out alignments/liver_align_illumina_R1.sam
+magicblast -query trimmed_liver_illumina_R2.fastq -db horse_reference -infmt fastq -out alignments/liver_align_illumina_R2.sam
+
+echo "*******************************************************************************************"
+echo "Finish alignments!"
+echo "*******************************************************************************************"
+
+# deactivate conda environment
 conda deactivate
